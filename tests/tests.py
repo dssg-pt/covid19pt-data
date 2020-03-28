99395385cfb5c313a747404a4449040243c6114a
paulomaia20 <up201406159@fe.up.pt>
2020-03-28 20:58:47 +0000

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,9 @@
 import pytest
 import pandas as pd
 
-def first_test():
+def test_first():
     data = pd.read_csv('data.csv')
+	
+    print(data)
 
     assert data['confirmados'].iloc[0] == 0, "CSV was changed"
