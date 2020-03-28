21cc98793d5104658111c6b4debdffb5b322bc4f
paulomaia20 <up201406159@fe.up.pt>
2020-03-28 14:39:04 +0000

    Action test

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+import requests  # noqa We are just importing this to prove the dependency installed correctly
+import pandas as pd
+
+def main():
+    data = pd.read_csv('data.csv')
+
+	assert data['confirmados'].iloc[0] == 0
+
+
+
+if __name__ == "__main__":
+    main()
