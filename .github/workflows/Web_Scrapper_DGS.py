29bb41f4d7857073b0aa3fbeac96092d6b17f90b
Luis Guilherme Sousa de Oliveira <lgsoliveira@gmail.com>
2020-06-04 09:20:41 +0100

    Update Web_Scrapper_DGS.py

--- .github/workflows/Web_Scrapper_DGS.py
+++ .github/workflows/Web_Scrapper_DGS.py
@@ -7,8 +7,12 @@ Created on Thu May 28 18:17:11 2020
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
-from datetime import datetime, timedelta
+from datetime import datetime
 from pathlib import Path
+import fileinput
+import locale
+locale.setlocale(locale.LC_TIME, "pt_PT")
+import sys
 
 # Get the link of TODAY
 def links_of_day(path,text_to_find,day):
@@ -27,8 +31,8 @@ def links_of_day(path,text_to_find,day):
 def get_latest_day():
     path = Path(__file__).resolve().parents[2]
     file=path / 'data.csv'
-    df= pd.read_csv(file).sort_values(by='data',ascending=False)
-    day=df.iloc[0,0]
+    df= pd.read_csv(file)
+    day=df.iloc[-1,0]
     return day
 
 #Save on .txt file
@@ -38,16 +42,29 @@ def save_link_txt(text_to_save):
     with open(file,'w') as f:
         f.write(text_to_save)
 
+def update_readme(date_now):
+    path = Path(__file__).resolve().parents[2]
+    filepath = path / 'README.md'
+    search_string = "📅️ **Última actualização**:"
+    new_string = "📅️ **Última actualização**: {} de {} de {}, {}\n".format(date_now.day, date_now.strftime('%B').capitalize(), date_now.year, date_now.strftime('%H:%M'))
+
+    for line in fileinput.input([filepath], inplace=True):
+        if line.strip().startswith(search_string):
+            line = new_string
+        sys.stdout.write(line)
+
 if __name__ == '__main__':
+    
     # Today
     date=(datetime.today())
-    
-    link=links_of_day('https://covid19.min-saude.pt/relatorio-de-situacao/',"Relatório de Situação",date)
 
+    # The link
+    link=links_of_day('https://covid19.min-saude.pt/relatorio-de-situacao/', "Relatório de Situação", date)
+
+    # The latest report in the file data.csv
     latest=get_latest_day()
-    # Check if latest day on data.csv is today and if the link exists
-    if ( datetime.strptime(latest, '%d/%m/%y')!=date.strftime("%d/%m/%Y")) & (link!=''):
-        save_link_txt(link)
 
-    
-        
+    # Check if latest day on data.csv is today and if the link exists
+    if ( datetime.strptime(latest, '%d-%m-%Y') != date.strftime("%d-%m-%Y")) & (link != ''):
+        save_link_txt(link)
+        update_readme(datetime.now())
