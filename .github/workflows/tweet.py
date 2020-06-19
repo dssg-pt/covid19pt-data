09e98d4356cab67196c4b423d35f551a047650b1
Helena Faria <hmgfaria@gmail.com>
2020-06-19 11:27:32 +0100

    Update

--- .github/workflows/tweet.py
+++ .github/workflows/tweet.py
@@ -7,7 +7,6 @@ from pathlib import Path
 import tweepy 
   
 # Login
-
 consumer_key = os.environ['TWITTER_CONSUMER_KEY']
 consumer_secret = os.environ['TWITTER_CONSUMER_SECRET']
 access_token = os.environ['TWITTER_ACCESS_TOKEN']
