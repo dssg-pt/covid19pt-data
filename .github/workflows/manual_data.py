f89b064cefede98fbb94f795f4418e0cad87d8f5
Bruno Rodrigues <bruno.rodrigues@litux.org>
2021-04-25 15:24:06 +0100

    dados manuais 25-04-2021

--- .github/workflows/manual_data.py
+++ .github/workflows/manual_data.py
@@ -5,37 +5,37 @@ from pathlib import Path
 
 if __name__ == "__main__":
 
-    today = "24-04-2021"
+    today = "25-04-2021"
 
     # boletim
-    ativos = 24628
-    recuperados = 792377
-    obitos = 16959
-    vigilancia = 23802
-    confirmados = 833964
-    confirmados_novos = 567
+    ativos = 24792
+    recuperados = 792685
+    obitos = 16965
+    vigilancia = 24313
+    confirmados = 834442
+    confirmados_novos = 478
 
-    confirmados_arsnorte = 334977
-    obitos_arsnorte = 5335
-    confirmados_arscentro = 118422
-    obitos_arscentro = 3009
-    confirmados_arslvt = 315624
-    obitos_arslvt = 7190
-    confirmados_arsalentejo = 29671
+    confirmados_arsnorte = 335149
+    obitos_arsnorte = 5337
+    confirmados_arscentro = 118464
+    obitos_arscentro = 3011
+    confirmados_arslvt = 315758
+    obitos_arslvt = 7191
+    confirmados_arsalentejo = 29722
     obitos_arsalentejo = 970
-    confirmados_arsalgarve = 21533
-    obitos_arsalgarve = 356
-    confirmados_acores = 4650
+    confirmados_arsalgarve = 21563
+    obitos_arsalgarve = 357
+    confirmados_acores = 4664
     obitos_acores = 31
-    confirmados_madeira = 9087
+    confirmados_madeira = 9122
     obitos_madeira = 68
 
-    internados, internados_uci = 342, 98
+    internados, internados_uci = 348, 98
 
-    confirmados_m, confirmados_f = 378305, 455353
+    confirmados_m, confirmados_f = 378523, 455613
     confirmados_desconhecido = 306
 
-    obitos_m, obitos_f = 8909, 8050
+    obitos_m, obitos_f = 8909, 8056
 
     incidencia_nacional, incidencia_continente = 72.1, 68.3
     rt_nacional, rt_continente = 0.98, 0.99
