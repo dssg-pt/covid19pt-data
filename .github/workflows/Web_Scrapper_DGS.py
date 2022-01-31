aa684cfaed9c0d687bf8ec683695d1a3b7a36a3e
Bruno D. Rodrigues <bruno.rodrigues@litux.org>
2022-01-31 18:40:10 +0000

    Get daily report from the header of the covid website - only the last day is available

--- .github/workflows/Web_Scrapper_DGS.py
+++ .github/workflows/Web_Scrapper_DGS.py
@@ -1,84 +1,39 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu May 28 18:17:11 2020
-
 @author: LgOliveira
 """
-import pandas as pd
-from bs4 import BeautifulSoup
+import re
 import requests
-from datetime import datetime, timedelta
 from pathlib import Path
-import fileinput
+
 import locale
 locale.setlocale(locale.LC_TIME, "pt_PT.UTF-8")
-import sys
 
-# Get the link of TODAY
-def links_of_day(path,text_to_find,day):
-    date=day.strftime("%Y%m%d")
-    with requests.get(path) as r:
-        soup=BeautifulSoup(r.text,"lxml")
-        
-        for link in soup.find_all('a'):
-            l=link.get('href')
-            if l and text_to_find in l and date in l and l.endswith(".pdf"):
-                return l
-                
-    return None
-
-# Get the latest day on the data.csv file
-def get_latest_day():
-    path = Path(__file__).resolve().parents[2]
-    #file=path / 'data.csv'
-    #df= pd.read_csv(file)
-    #day=df.iloc[-1,0]
-    pdf_folder = path / 'dgs-reports-archive/'
-
-    pdf_files = list(pdf_folder.glob('*.pdf'))
-
-    pdf_files.sort(key=lambda p: p.stem, reverse=True)
-
-    last_pdf = str(pdf_files[0])
-    # print(f'last_pdf={last_pdf}')
-    day = last_pdf[-14:-4].replace('_', '-')
-
-    return day
-
-#Save on .txt file
-def save_link_txt(text_to_save):
-    path = Path(__file__).resolve().parents[1]
-    file=path / 'report_link.txt'
-    with open(file,'w') as f:
-        f.write(text_to_save)
-
-def update_readme(date_now):
-    path = Path(__file__).resolve().parents[2]
-    filepath = path / 'README.md'
-    search_string = "📅️ **Última actualização**:"
-    new_string = "📅️ **Última actualização**: {} de {} de {}, {}\n".format(date_now.day, date_now.strftime('%B').capitalize(), date_now.year, date_now.strftime('%H:%M'))
-
-    for line in fileinput.input([filepath], inplace=True):
-        if line.strip().startswith(search_string):
-            line = new_string
-        sys.stdout.write(line)
 
 if __name__ == '__main__':
-    
-    # Today
-    DAYS_OFFSET = int(sys.argv[1]) if len(sys.argv) > 1 else 0
-    date=(datetime.today() - timedelta(days=DAYS_OFFSET))
 
-    # The link
-    link=links_of_day('https://covid19.min-saude.pt/relatorio-de-situacao/', "DGS_boletim", date)
+    url = 'https://covid19.min-saude.pt'
 
-    # The latest report in the file data.csv
-    latest=get_latest_day()
+    response = requests.get(url=url)
+    if response.status_code != 200:
+        raise ValueError('Unable to retrieve data from covid site. Error %s: $s' % response.status_code, response.text)
 
-    # Check if latest day on data.csv is not today and if the link exists
-    if link and (latest != date.strftime("%d-%m-%Y")):
-        save_link_txt(link)
-        update_readme(datetime.now())
-        print(f"UPDATED latest={latest} today={date.strftime('%d-%m-%Y')} link={link}")
-    else:
-        print(f"NO CHANGES latest={latest} today={date.strftime('%d-%m-%Y')} link={link}")
+    # <a href="https://covid19.min-saude.pt/wp-content/uploads/2022/01/700_DGS_boletim_20220131.pdf" target="_blank">Ponto de Situação (31-01-2022)</a>
+    matches = re.search((
+        r'(https://covid19.min-saude.pt/wp-content/uploads/[0-9]+/[0-9]+/([0-9]+_DGS_boletim_([0-9]+)\.pdf))'
+    ), response.text, re.MULTILINE | re.IGNORECASE)
+
+    if not matches:
+        print(response.text)
+        raise("Link not found")
+    link = matches.group(1)
+
+    path = Path(__file__).resolve().parents[1]
+    file=path / 'report_link.txt'
+    with open(file,'r') as f:
+        old_link = f.readline()
+
+    if old_link != link:
+        with open(file,'w') as f:
+            f.write(link)
+        print(f"UPDATED latest={old_link} link={link}")
--- .github/workflows/scripts/check_pdf_update.py
+++ .github/workflows/scripts/check_pdf_update.py
@@ -1,4 +1,3 @@
-import json
 import pathlib
 import sys
 import re
@@ -9,46 +8,29 @@ DEBUG = len(sys.argv) > 1
 
 
 def get_most_recent_covid_file():
-
     pdf_folder = pathlib.Path('dgs-reports-archive/')
-
     pdf_files = list(pdf_folder.glob('*.pdf'))
-
     pdf_files.sort(key=lambda p: p.stem, reverse=True)
-
     last_pdf = str(pdf_files[0])
     if DEBUG: print(f'last_pdf={last_pdf}')
     last_date = last_pdf[-14:-4].replace('_', '-')
     if DEBUG: print(f'last_date={last_date}')
-
     return last_date
 
 
 def get_covid_data_from_api():
-
-    url = 'https://covid19.min-saude.pt/relatorio-de-situacao/'
+    url = 'https://covid19.min-saude.pt'
 
     response = requests.get(url=url)
-
     if response.status_code != 200:
         raise ValueError('Unable to retrieve data from covid site. Error %s: $s' % response.status_code, response.text)
 
-    #if DEBUG: print(response.text)
-    matches = re.search(
-        (
-            r'Relat.rio de Situa..o n.\s*[0-9]+\s*\|'
-            r'\s*([0-3][0-9])'
-            r'\s*(?:<[^>]+?>)?'
-            r'\s*/'
-            r'\s*(?:<[^>]+?>)?'
-            r'\s*([0-1][0-9])'
-            r'\s*/'
-            r'\s*(?:<[^>]+?>)?'
-            r'\s*(202[0-9])'
-            r'\s*[^<]*?</a>'
+    # <a href="https://covid19.min-saude.pt/wp-content/uploads/2022/01/700_DGS_boletim_20220131.pdf" target="_blank">Ponto de Situação (31-01-2022)</a>
+    matches = re.search((
+        r'wp-content/uploads/[0-9]+/[0-9]+/([0-9]+_DGS_boletim_([0-9]{8})\.pdf)'
     ), response.text, re.MULTILINE | re.IGNORECASE)
     if DEBUG: print(f"matches={matches}")
-    latest_date = f"{matches.group(1)}-{matches.group(2)}-{matches.group(3)}"
+    latest_date = f"{matches.group(2)[6:8]}-{matches.group(2)[4:6]}-{matches.group(2)[0:4]}"  # DD-MM-YYYY
     if DEBUG: print(f"latest_date={latest_date}")
 
     return latest_date
@@ -57,7 +39,6 @@ def get_covid_data_from_api():
 if __name__ == '__main__':
 
     current_data = get_most_recent_covid_file()
-
     new_data = get_covid_data_from_api()
 
     try:
