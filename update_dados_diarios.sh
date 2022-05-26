045752352fbd9e347fd3e6f00fd50bc22df45832
Bruno Rodrigues <bruno.rodrigues@litux.org>
2022-05-26 23:39:01 +0100

    dados diarios

--- update_dados_diarios.sh
+++ update_dados_diarios.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+git pull
+
 curl --connect-timeout 3 -gsN 'https://covid19.min-saude.pt/numero-de-novos-casos-e-obitos-por-dia/' | perl -ne 's/http/\nhttp/g;print' | perl -ne '/(http[^"]+\.xls[^"]*)/&&print"$1\n"' | sort -n | uniq | while read a ; do b=${a:0:78};c=$(basename $b); [ -r "dados_diarios/$c.xlsx" ] && continue ; echo $a ;  curl --connect-timeout 3 -o "dados_diarios/$c.xlsx" "$a" ; done 
 
 python3 .github/workflows/update_dados_diarios.py $(ls dados_diarios/*.xls* | sort -n | tail -1)
 
 git add dados_diarios/*.xls*
 git add dados_diarios.csv
+git add update_dados_diarios.sh
 
 if [ "x$(git status | grep -E "modified|new file)")" != "x" ] ; then
 	git status
