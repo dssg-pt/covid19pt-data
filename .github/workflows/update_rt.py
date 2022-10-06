f0540358fec63ba232f48aad0589bc3e993185e8
Bruno D. Rodrigues <bruno.rodrigues@litux.org>
2022-10-06 19:42:16 +0100

    Allow update_rt to work even if a file is missing (currently, Madeira misses the 10/October XLSX and the 09/September is also gone) by reading the current file and keeping the previous values

--- .github/workflows/update_rt.py
+++ .github/workflows/update_rt.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 from pathlib import Path
 
-URL = "http://www.insa.min-saude.pt/wp-content/uploads/{year:04d}/{month:02d}/Rt_{region}.xlsx"
+URL = "https://www.insa.min-saude.pt/wp-content/uploads/{year:04d}/{month:02d}/Rt_{region}.xlsx"
 
 REGIONS = (
     ("nacional", "nacional", ),
@@ -18,21 +18,31 @@ REGIONS = (
 )
 
 if __name__ == "__main__":
+    PATH_TO_CSV = str(Path(__file__).resolve().parents[2] / "rt.csv")
+    df = pd.read_csv(PATH_TO_CSV, parse_dates=[0], index_col=[0], infer_datetime_format=True)
+    # Rename index column 'data' to 'Data'
+    df.index.names = ["Data"]
+
     today = datetime.datetime.today()
     year, month = int(today.strftime("%Y")), int(today.strftime("%m"))
     diff = 0
-    df = None
     for region in REGIONS:
         try:
             url = URL.format(year=year, month=month, region=region[1])
             print(f"Retrieving {'last' if diff else 'this'} month {year}-{month} for {region}: {url}")
             data = pd.read_excel(url, index_col="Data")
-        except:
-            (year, month) = (year - 1, 12) if month == 1 else (year, month-1)
-            diff += 1
-            url = URL.format(year=year, month=month, region=region[1])
-            print(f"Retrieving last month {year}-{month} for {region}: {url}")
-            data = pd.read_excel(url, index_col="Data")
+        except Exception as e:
+            print(f"Error {e}")
+            try:
+                (year, month) = (year - 1, 12) if month == 1 else (year, month-1)
+                diff += 1
+                url = URL.format(year=year, month=month, region=region[1])
+                print(f"Retrieving last month {year}-{month} for {region}: {url}")
+                data = pd.read_excel(url, index_col="Data")
+            except Exception as e:
+                print(f"Error2 {e}")
+                print(f"Bad luck, region={region} is missing")
+                continue
 
         # 'Rt_número_de_reprodução', 'limite_inferior_IC95%', 'limite_superior_IC95%']
         data.columns = [
@@ -40,14 +50,12 @@ if __name__ == "__main__":
             f"rt_95_inferior_{region[0]}",
             f"rt_95_superior_{region[0]}",
         ]
-        if df is None:
-            df = data
-        else:
-            df = pd.merge(df, data, how="outer", on=["Data"])
+        df[data.columns] = data
 
-    # Rename index column 'Data' to 'data'
+    # end for regions
+
+    # Rename index column 'Data' back to 'data'
     df.index.names = ["data"]
 
     # save to .csv
-    PATH_TO_CSV = str(Path(__file__).resolve().parents[2] / "rt.csv")
     df.to_csv(PATH_TO_CSV, index=True, line_terminator="\n")
