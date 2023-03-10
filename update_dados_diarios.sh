4fc30acaa56b18c827b06c03b0511f69375a969e
Bruno D. Rodrigues <bruno.rodrigues@litux.org>
2023-03-10 15:58:02 +0000

    dados diarios

--- update_dados_diarios.sh
+++ update_dados_diarios.sh
@@ -2,7 +2,18 @@
 
 git pull
 
-curl --connect-timeout 3 -gsN 'https://covid19.min-saude.pt/numero-de-novos-casos-e-obitos-por-dia/' | perl -ne 's/http/\nhttp/g;print' | perl -ne '/(http[^"]+\.xls[^"]*)/&&print"$1\n"' | sort -n | uniq | while read a ; do b=${a:0:78};c=$(basename $b); [ -r "dados_diarios/$c.xlsx" ] && continue ; echo $a ;  curl --connect-timeout 3 -o "dados_diarios/$c.xlsx" "$a" ; done 
+curl --connect-timeout 3 -gsN 'https://covid19.min-saude.pt/numero-de-novos-casos-e-obitos-por-dia/' \
+	| perl -ne 's/http/\nhttp/g;print' \
+	| perl -ne '/(http[^"]+\.xls[^"]*)/&&print"$1\n"' \
+	| sort -n | uniq \
+	| while read a ; do 
+		b=${a:0:78}
+		[ "x${b:77}" = "xx" ] && b=${b:0:72}-${b:72:2}-${b:74:2}
+		c=$(basename $b)
+		[ -r "dados_diarios/$c.xlsx" ] && continue
+		echo $a
+		curl --connect-timeout 3 -o "dados_diarios/$c.xlsx" "$a"
+	done 
 
 python3 .github/workflows/update_dados_diarios.py $(ls dados_diarios/*.xls* | sort -n | tail -1)
 
